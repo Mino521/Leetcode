2318db6058c411078273f5dfb6ab928151298490
Zeming Wang <wzm970527@gmail.com>
2021-07-19 15:15:18 +0800

    Optimize solution for Q139

--- 139-Word Break/Solution.java
+++ 139-Word Break/Solution.java
@@ -3,10 +3,12 @@ class Solution {
         Set<String> set = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length()+1];
         dp[0] = true;
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i+1; j <= s.length(); j++){
-                if(dp[i] && set.contains(s.substring(i,j)))
-                    dp[j] = true;
+        for (int right = 1; right <= s.length(); right++) {
+            for (int left = right - 1; left >= 0; left--) {
+                if (dp[left] && set.contains(s.substring(left, right))) {
+                    dp[right] = true;
+                    break;
+                }
             }
         }
         return dp[s.length()];
