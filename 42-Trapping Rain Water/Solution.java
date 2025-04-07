1c488d31884eef972c3eb449f134df009c3ee0aa
Zeming Wang <wzm970527@gmail.com>
2025-04-07 21:08:53 +0800

    Q42 Solution

--- 42-Trapping Rain Water/Solution.java
+++ 42-Trapping Rain Water/Solution.java
@@ -22,3 +22,25 @@ class Solution {
         return ans;
     }
 }
+
+class Solution2 {
+    public int trap(int[] height) {
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        int maxh = 0;
+        for(int i = 0; i < height.length; i++){
+            left[i] = Math.max(maxh, height[i]);
+            maxh = left[i];
+        }
+        maxh = 0;
+        for(int i = height.length-1; i >= 0; i--){
+            right[i] = Math.max(maxh, height[i]);
+            maxh = right[i];
+        }
+        int res = 0;
+        for(int i = 0; i < height.length; i++){
+            res += (Math.min(left[i], right[i]) - height[i]);
+        }
+        return res;
+    }
+}
