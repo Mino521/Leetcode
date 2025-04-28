d4fb4928734312d6f789e1666860ad5f8fe3b3fa
Zeming Wang <wzm970527@gmail.com>
2025-04-28 21:38:31 +0800

    Q124 better solution

--- 124-Binary Tree Maximum Path Sum/Solution.java
+++ 124-Binary Tree Maximum Path Sum/Solution.java
@@ -32,3 +32,34 @@ class Solution {
         return max1;
     }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution2 {
+
+    int maxLength = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxValue(root);
+        return maxLength;
+    }
+
+    private int maxValue(TreeNode root) {
+        if(root == null) return 0;
+        int left = maxValue(root.left);
+        int right = maxValue(root.right);
+        maxLength = Math.max(maxLength, left + right + root.val);
+        return Math.max(0, root.val+Math.max(left, right));
+    }
+}
